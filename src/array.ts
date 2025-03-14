ae24a4e4ec27f7e7bda1b2eeba8ca30af0074661
hanaarena <hanaarena@gmail.com>
2025-03-14 22:33:33 +0900

    feat(array): add groupBy

--- src/array.ts
+++ src/array.ts
@@ -11,3 +11,15 @@ export function shuffleArray<T = any>(arr: T[], outputLength?: number): T[] {
   }
   return array;
 }
+
+export function groupBy<T>(input: T[], key: string): Record<string, T[]> {
+	const result = input.reduce<Record<string, any[]>>((acc, currentValue: any) => {
+		const groupKey = currentValue[key]
+		if (!acc[groupKey]) {
+			acc[groupKey] = []
+		}
+		acc[groupKey].push(currentValue)
+		return acc
+	}, {})
+  return result
+}
